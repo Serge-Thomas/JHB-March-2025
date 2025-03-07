6d26ecc21f1bf0b113fadfff5f10ff5a4da3a674
Serge-Thomas <79256268+Serge-Thomas@users.noreply.github.com>
2025-03-07 12:16:14 +0200

    Add files via upload

---
+++ Level 1/Code Examples/dataTypes.py
@@ -0,0 +1,54 @@
+# 🔹 Text (String - str) → Words and sentences (e.g., "Hello", "Python")
+# 🔹 Whole Numbers (Integer - int) → Numbers without decimals (e.g., 10, -5)
+# 🔹 Decimal Numbers (Float - float) → Numbers with decimals (e.g., 3.14, 2.5)
+# 🔹 True/False (Boolean - bool) → Yes/No values (e.g., True, False)
+
+# Example 1
+# first_name = input("Enter your first name: ")
+# last_name = input("Enter your last name: ")
+
+# # full_name = first_name + last_name
+# full_name = first_name + " * " + last_name
+
+# print(full_name)
+
+
+# Example 2
+
+
+# "H | E | L | L | O"
+#  0   1   2   3   4
+
+# -6 -5  -4  -3  -2  -1
+# B | I | T | C | H | E
+# 0   1   2   3   4   5
+
+# greeting = "BITCHE"
+
+# Negative indexing
+# print(greeting[-2])   # ==> 'H'
+
+# print(greeting[-3:-1]) 
+
+
+# Indexing ==> greeting[] 
+# print(greeting[2])
+
+# Slicing ==> greeting[start point:ending point]
+# print(greeting[3:5])
+
+# print(greeting[3:6])
+
+# print(greeting[3:])
+
+
+# REVERSING A STRING
+# [::-1]
+
+# [starting point : ending point : how many steps it should take]
+
+greeting = "BITCHEs"
+
+# print(greeting[::3])  Steps
+
+print(greeting[::-1])
---
+++ Level 1/Code Examples/variables_exp.py
@@ -0,0 +1,33 @@
+# name = "Serge"
+# print("user_name")
+# # user_name = "Ingrid"
+
+# # Input - taking in information
+# user_name = input("What is your name ? ")
+# user_age = input("How old are you ? ")
+
+# # Output - displaying information
+# print(user_name)
+# print(user_age)
+
+# input("enter your name serge: ")
+
+# random_variable = input("Enter something: ")
+
+# print(random_variable)
+
+# print("Hello " + user_name)
+# print("You must be " + user_age + " years old, smh still a kid")
+
+# Working with numbers and input
+# We have to convert these values using float() and int()
+
+# int()
+user_age = int(input("How old are you ? "))
+
+print("You are currently " + str(user_age) +
+      " years old. Next year you will be " + str(user_age + 1))
+
+# Formatter ==> f-string
+print(f"You are currently {user_age} years old.\
+ Next year you will be {user_age + 1}")
